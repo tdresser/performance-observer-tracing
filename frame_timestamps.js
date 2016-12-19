c38211357131e29ff67a2087f3291c68a7924ece
Tim Dresser <tdresser@chromium.org>
2016-12-19 14:56:01 -0500

    Add long frame logic

---
+++ frame_timestamps.js
@@ -0,0 +1,16 @@
+(function frameTimestamps() {
+  var lastRafTime = 0;
+  function raf(time) {
+    window.requestAnimationFrame(raf);
+    if (time - lastRafTime > 50) {
+      performance.emit({
+        name: "long Frame",
+        entryType: "longFrame",
+        duration: time - lastRafTime,
+        startTime: lastRafTime
+      })
+    }
+    lastRafTime = time;
+  }
+  window.requestAnimationFrame(raf);
+})();
