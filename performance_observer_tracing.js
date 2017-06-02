c12a7705a49dae5dd2a1acd3f66dd906cb3b4fb5
Tim Dresser <tdresser@chromium.org>
2017-06-02 11:34:24 -0400

    Just performance observer tracing

--- performance_observer_tracing.js
+++ performance_observer_tracing.js
@@ -1,53 +1,9 @@
 (function () {
   'use strict';
 
-  function download(filename, text) {
-    const blob = new Blob([text], {type: 'text/plain'});
-
-    var element = document.createElement('a');
-    element.setAttribute('href', window.URL.createObjectURL(blob));
-    element.setAttribute('download', filename);
-
-    element.style.display = 'none';
-    document.body.appendChild(element);
-    element.click();
-    document.body.removeChild(element);
-  }
-
   const currentTrace = [];
 
   let id = 0;
-  let bind_id = 0;
-
-  function handleEventEntry(entry, primaryTraceEvent) {
-    const queueingTimeName = entry.name + '::event queueing time';
-    const queueingTimePid = 'Input::' + entry.name + '::Queueing';
-    const flowEventStart = {
-      name: queueingTimeName,
-      ph: 'X',
-      dur: 0,
-      pid: queueingTimePid,
-      cat: entry.entryType,
-      bind_id: '0x' + bind_id.toString(16),
-      ts: entry.startTime * 1000,
-      flow_out: true,
-    };
-
-    const flowEventEnd = {
-      name: primaryTraceEvent.name,
-      ph: 'X',
-      dur: 0,
-      pid: primaryTraceEvent.pid,
-      cat: entry.entryType,
-      bind_id: '0x' + bind_id.toString(16),
-      ts: entry.eventHandlersBegin * 1000,
-      flow_in: true,
-    };
-
-    bind_id++;
-    currentTrace.push(flowEventStart);
-    currentTrace.push(flowEventEnd);
-  }
 
   const observer = new PerformanceObserver((list) => {
     for (let entry of list.getEntries()) {
@@ -64,22 +20,8 @@
       case 'measure':
         traceEvent.pid = 'Measures';
         break;
-      case 'event':
-        traceEvent.pid = 'Input::' + entry.name;
-        break;
       default:
         traceEvent.pid = 'Primary';
-      }
-
-      if (entry.entryType == 'event') {
-        // We display the queueing time in handleEventEntry, remove it from the
-        // primary event slice.
-        traceEvent.ts = entry.eventHandlersBegin * 1000;
-        entry.duration = entry.eventHandlersEnd - entry.eventHandlersBegin;
-        // Use entry name first to sort event types next to their queueing times.
-        traceEvent.name = entry.name + '::' + entry.entryType;
-
-        handleEventEntry(entry, traceEvent);
       }
 
       if (entry.entryType == 'resource') {
@@ -124,16 +66,13 @@
   observer.observe({entryTypes: [
     'resource',
     'navigation',
-    'event',
-    'frame',
-    'styleupdate',
     'paint',
     'longtask',
     'mark',
     'measure'
   ]});
 
-  window.downloadTrace = function() {
-    download('performance_observer_trace.json', JSON.stringify(currentTrace));
+  window.getPerformanceObserverTrace = function() {
+    return currentTrace;
   }
 })();
